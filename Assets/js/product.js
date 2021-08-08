1b979be8fa2197b64046ca584a74a35266759bb5
yeswanth <yeswanth15750@gmail.com>
2021-08-08 19:58:21 +0530

    productSpec page is updated

--- Assets/js/product.js
+++ Assets/js/product.js
@@ -1,18 +1,36 @@
-
-var images = [{ imageUrl: "add-user.png", name: "product1", price: "100" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "gift.png", name: "product4", price: "400" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }];
+// getting data from server
+function product(){
+    const url =("https://product-mock-api.herokuapp.com/giftshopapp/api/v1/products");
+    axios.get(url).then(res =>{
+        console.log(res.data);
+        var images =res.data;
+    
+// var images = [{ imageUrl: "add-user.png", name: "product1", price: "100" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }, { imageUrl: "gift.png", name: "product4", price: "400" }, { imageUrl: "doeramon.jpg", name: "product3", price: "300" }];
 // displaying images 
         let content = "";
         var count = 0;
         for (let img of images) {
 
-            content = content +
-             `
-            <div class="product">
+            // content = content +
+            //  `
+            // <div class="product" id="${img.id}">
                 
-                    <a href="productSpec.html"><img class="productImg" src="Assets/Images/${img.imageUrl}" id="productImg" alt=""></a>
-                <p class="productName" id="productName">${img.name}</p>
-                <p class="productPrice" id="productPrice" >${img.price}</p>
-            </div>`;
+            //         <a href="productSpec.html"><img class="productImg" src="Assets/Images/${img.image_url}" id="productImg" alt=""></a>
+            //     <p class="productName" id="productName">${img.name}</p>
+            //     <p class="productPrice" id="productPrice" >${img.price}</p>
+
+                
+            // </div>`;
+            content = content + `<div class="product" id="${img.id}">
+            <form onsubmit="addToCart(${img.id})">
+       <img class="productImg" src="Assets/Images/${img.image_url}" onClick=specOpen(${img.id}) id="productImg" alt="">    
+    <p class="productName" id="productName">${img.name}</p>
+    <p class="productPrice" id="productPrice" >${img.price}</p>
+    <p>${img.id}</p>
+    
+    <button type="submit">add to cart</button>
+</form>
+</div>`;
             count = count + 1;
             if (count == 4) {
                 content = content + `<br>`;
@@ -21,3 +39,22 @@ var images = [{ imageUrl: "add-user.png", name: "product1", price: "100" }, { im
         }
 
         document.querySelector("#productContainer").innerHTML = content;
+    })
+}
+// global variable id
+let productId;
+function specOpen(ids){
+    
+    // alert("im in addToCart");
+    // alert(ids);
+
+localStorage.setItem("productSpec",ids);
+// alert("check");
+window.location.href="productSpec.html";
+// alert(id);
+}
+
+
+
+
+product();
---
+++ Assets/js/productSpec.js
@@ -0,0 +1,31 @@
+// product spec page
+
+function productSpec(){
+    var Id= localStorage.getItem("productSpec");    
+    // alert("showing id in product specific page");
+    // alert(Id);
+    
+    const url =(`https://product-mock-api.herokuapp.com/giftshopapp/api/v1/products/${Id}}`);
+    axios.get(url).then(res =>{
+        console.log(res.data.image_url);
+        let datas =res.data;
+        // alert(datas.image_url);
+        let content =`
+        <img src="Assets/Images/${datas.image_url}" alt="">
+        <p>${datas.name}</p>
+        <br>
+        <p>${datas.price}</p>
+        <br>
+        <p>product description......</p>
+        <form onsubmit="tocart()">
+            <button type="submit">add to cart</button>
+        </form>`;
+        document.querySelector(".productSpec").innerHTML=content;
+        
+        
+    
+    });
+alert(datas.image_url);
+    var reset =0;
+    localStorage.setItem("productSpec",reset);
+}
