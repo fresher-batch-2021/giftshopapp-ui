2153f5edfd672fafea92ec6214445a0f520f1123
yeswanth <yeswanth15750@gmail.com>
2021-09-02 13:47:29 +0530

    sending aboutUs

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -3,7 +3,7 @@ const routes = [
     { path: 'product.html' },
     { path: 'productSpec.html' },
     { path: 'cart.html', roles: ["USER","ADMIN"] },
-    { path: 'aboutUs.html' },
+    { path: 'AboutUs.html' },
     { path: 'login.html' },
     { path: 'register.html' },
     { path: 'myOrder.html' , roles: ["USER","ADMIN"]},    
